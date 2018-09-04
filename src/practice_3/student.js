a2e85b80d5d82ab9a422e33fac3bce124e75bcb8
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 16:13:53 +0800

    practice_3: complete Person class, Student class and Worker class

--- src/practice_3/student.js
+++ src/practice_3/student.js
@@ -1,1 +1,13 @@
+import Person from '../../src/practice_3/person.js';
+
+export default class Student extends Person{
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return `I am a Student. I am at Class ${this.klass}.`
+    }
+}
 
--- src/practice_3/worker.js
+++ src/practice_3/worker.js
@@ -1,1 +1,12 @@
+import Person from '../../src/practice_3/person.js';
+
+export default class Worker extends Person{
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    introduce() {
+        return `I am a Worker. I have a job.`;
+    }
+}
 
