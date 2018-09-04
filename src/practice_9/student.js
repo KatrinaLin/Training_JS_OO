354c850db0694dfa956b21a73d79a259164c341a
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 19:13:26 +0800

    practice_9: append student to a class

--- src/practice_9/student.js
+++ src/practice_9/student.js
@@ -1,1 +1,20 @@
+import Person from '../../src/practice_9/person.js';
+
+export default class Student extends Person{
+    constructor(id, name, age, klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    intro_basic() {
+        return `${super.introduce()} I am a Student.`;
+    }
+
+    introduce() {
+        let msg = (this.klass.leader && this.klass.leader.id === this.id) ? "Leader of" : "at";
+        return `${this.intro_basic()} I am ${msg} Class ${this.klass.number}.`
+    }
+}
+
+
 
--- src/practice_9/teacher.js
+++ src/practice_9/teacher.js
@@ -1,1 +1,21 @@
+import Person from '../../src/practice_9/person.js';
+
+export default class Teacher extends Person{
+    constructor(id, name, age, klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    intro_basic() {
+        return `${super.introduce()} I am a Teacher.`;
+    }
+
+    introduce() {
+        let msg = this.klass ? 'Class ' + this.klass.number : 'No Class';
+        return `${this.intro_basic()} I teach ${msg}.`
+    }
+}
+
+
+
 
