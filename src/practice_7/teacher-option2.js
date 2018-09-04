ee91961e95efa371ddf5e38190c94b862066b7b0
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 19:06:45 +0800

    practice_7: determine whether a teacher teach a student

--- src/practice_7/teacher-option2.js
+++ src/practice_7/teacher-option2.js
@@ -1,1 +1,26 @@
+import Person from '../../src/practice_7/person.js';
+
+export default class Teacher extends Person{
+    // TODO: refactor
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    intro_basic() {
+        return `${super.introduce()} I am a Teacher.`;
+    }
+
+    introduce() {
+        let msg = this.klass ? 'Class ' + this.klass.number : 'No Class';
+        return `${this.intro_basic()} I teach ${msg}.`
+    }
+
+    introduceWith(student) {
+        let teachMsg = student.klass.number === this.klass.number ? "" : "don't ";
+        return `${this.intro_basic()} I ${teachMsg}teach ${student.name}.`;
+    }
+}
+
+
 
--- src/practice_7/teacher.js
+++ src/practice_7/teacher.js
@@ -1,1 +1,24 @@
+import Person from '../../src/practice_7/person.js';
+
+export default class Teacher extends Person{
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    intro_basic() {
+        return `${super.introduce()} I am a Teacher.`;
+    }
+
+    introduce() {
+        let msg = this.klass ? 'Class ' + this.klass.number : 'No Class';
+        return `${this.intro_basic()} I teach ${msg}.`
+    }
+
+    introduceWith(student) {
+        let teachMsg = student.klass.number === this.klass.number ? "" : "don't ";
+        return `${this.intro_basic()} I ${teachMsg}teach ${student.name}.`;
+    }
+}
+
 
