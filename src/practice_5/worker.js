217963461b963806ea40904fdb7cd9229878a02f
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 16:33:20 +0800

    practice_5: complete Person class, Student class and Worker class, use introduce method

--- src/practice_5/worker.js
+++ src/practice_5/worker.js
@@ -1,1 +1,14 @@
+import Person from '../../src/practice_5/person.js';
+
+export default class Worker extends Person{
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Teacher. I have a job.`
+    }
+}
+
+
 
