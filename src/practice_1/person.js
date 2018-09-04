63d132c8108b4986786d817424379bb4af50cf40
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 15:51:24 +0800

    practice_1: initialize Person class

--- src/practice_1/person.js
+++ src/practice_1/person.js
@@ -1,1 +1,10 @@
+export default class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
+    introduce() {
+        return "My name is Tom. I am 21 years old."
+    }
+}
