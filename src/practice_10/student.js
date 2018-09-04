096dad3935fd7deb770e62356e24fc1d48e49ddc
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 21:17:52 +0800

    practice_10: display teacher's classes

--- src/practice_10/student.js
+++ src/practice_10/student.js
@@ -1,1 +1,17 @@
+import Person from '../../src/practice_10/person.js';
 
+export default class Student extends Person{
+    constructor(id, name, age, klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    intro_basic() {
+        return `${super.introduce()} I am a Student.`;
+    }
+
+    introduce() {
+        let msg = (this.klass.leader && this.klass.leader.id === this.id) ? "Leader of" : "at";
+        return `${this.intro_basic()} I am ${msg} Class ${this.klass.number}.`
+    }
+}
