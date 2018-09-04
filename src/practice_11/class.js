8b76c7cd77dfc2ade6d63111d0763adc530f84be
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 21:31:35 +0800

    practice_11: complete

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -1,1 +1,35 @@
+export default class Class {
+    constructor(number) {
+        this.number = number;
+    }
 
+    assignLeader(student) {
+        if (this.number === student.klass.number) {
+            this.leader = student;
+            if (this.teacher) {
+                console.log(`I am ${this.teacher.name}. I know ${student.name} become Leader of Class ${this.number}.`)
+            }
+        } else {
+            console.log("It is not one of us.")
+        }
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`
+    }
+
+    appendMember(student) {
+        student.klass = this;
+        if (this.teacher) {
+            console.log(`I am ${this.teacher.name}. I know ${student.name} has joined Class ${this.number}.`)
+        }
+    }
+
+    registerJoinListener(teacher) {
+        this.teacher = teacher;
+    }
+
+    registerAssignLeaderListener(teacher) {
+        this.teacher = teacher;
+    }
+}
--- src/practice_11/teacher.js
+++ src/practice_11/teacher.js
@@ -1,1 +1,22 @@
+import Person from '../../src/practice_11/person.js';
+
+export default class Teacher extends Person{
+    constructor(id, name, age, klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    intro_basic() {
+        return `${super.introduce()} I am a Teacher.`;
+    }
+
+    introduce() {
+        let msg = (this.klasses && this.klasses.length !== 0) ? 'Class ' + this.getClasses(this.klasses) : 'No Class';
+        return `${this.intro_basic()} I teach ${msg}.`
+    }
+
+    getClasses() {
+        return this.klasses.map(x => x.number).join(", ");
+    }
+}
 
