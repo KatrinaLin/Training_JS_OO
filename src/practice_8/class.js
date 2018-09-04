4b18b0295e16d5a392a79b7da8aa8903402cc3b0
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 19:07:08 +0800

    practice_8: assign leader for a class

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,16 @@
+export default class Class {
+    constructor(number) {
+        this.number = number;
+        this.leader;
+    }
 
+    assignLeader(student) {
+        if (this.number === student.klass.number) {
+            this.leader = student;
+        }
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`
+    }
+}
