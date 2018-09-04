c890ca2d50b08611671f43ba10a636faf2684f6f
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 16:27:34 +0800

    practice_4: complete Person class, Student class and Worker class, use basic_introduce method

--- src/practice_4/student.js
+++ src/practice_4/student.js
@@ -1,1 +1,14 @@
+import Person from '../../src/practice_4/person.js';
+
+export default class Student extends Person{
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return `${this.basic_introduce()} I am a Student. I am at Class ${this.klass}.`
+    }
+}
+
 
--- src/practice_4/worker.js
+++ src/practice_4/worker.js
@@ -1,1 +1,13 @@
+import Person from '../../src/practice_4/person.js';
+
+export default class Worker extends Person{
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    introduce() {
+        return `${this.basic_introduce()} I am a Worker. I have a job.`
+    }
+}
+
 
